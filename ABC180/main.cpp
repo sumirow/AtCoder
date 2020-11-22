c55a869c8fbb7792c07e1e02d7f41f64fb576864
sumirow <yumo3690@yahoo.co.jp>
2020-11-22 16:28:47 +0900

    20201122

--- ABC180/main.cpp
+++ ABC180/main.cpp
@@ -41,3 +41,21 @@ int main(){
 	printf("%.15f\n",u);
 	cout << c << endl;
 }
+
+//20201122 C
+#include <bits/stdc++.h>
+using ll = int64_t;
+using namespace std;
+
+int main(){
+	ll n;
+	cin >> n;
+	set<long> ans;
+	for(long i = 1; i*i<=n; i++){
+		if(n%i==0){
+			ans.insert(i);
+			ans.insert(n/i);
+		}
+	}
+	for (auto x:ans)cout<<x<<endl;
+}
